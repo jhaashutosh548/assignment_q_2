8e1bcfb168610e2b02a3c993aa24e3d8d7f1151b
jhaashutosh548 <131469144+jhaashutosh548@users.noreply.github.com>
2023-05-06 14:01:04 +0530

    Add files via upload

---
+++ Assignemnt_question_1 .py
@@ -0,0 +1,12 @@
+value = input('Enter the word or name: ')
+print('The input word is:',value)
+
+str1 = ''
+for i in value: #i = o  
+    str1 = i + str1 # E | d + E = dE | y + dE = ydE | o + ydE = oydE
+
+print('The output is:',str1)
+
+
+# word = input("enter a word :")
+# print(word[::-1])
